8f4552b85c266aa0e90d0a82bb36b3c98181f079
luwg <luwg@zzlc.com>
2019-05-23 17:48:53 +0800

    临时提交

--- src/main/java/com/youyoustudio/myhadoop/TestHDFS.java
+++ src/main/java/com/youyoustudio/myhadoop/TestHDFS.java
@@ -1,13 +1,13 @@
 package com.youyoustudio.myhadoop;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
+import java.io.*;
 
 public class TestHDFS {
     Configuration conf;
@@ -20,21 +20,44 @@ public class TestHDFS {
     }
 
     @After
-    public void close() throws IOException{
+    public void close() throws IOException {
         fileSystem.close();
     }
 
     @Test
-    public void mkdir()throws IOException{
+    public void mkdir() throws IOException {
+        Path path = new Path("/ooxx");
+        if (fileSystem.exists(path)) {
+            fileSystem.delete(path, true);
+        }
+        fileSystem.mkdirs(path);
+    }
 
-        try {
-            Path path = new Path("/user");
-            if (fileSystem.exists(path)) {
-                fileSystem.delete(path, true);
-            }
-            //fileSystem.mkdirs(path);
-        }catch (IOException e){
-            e.printStackTrace();
+    @Test
+    public void upload()throws IOException{
+        Path path = new Path("/ooxx/hello.txt");
+        FSDataOutputStream fsDataOutputStream = fileSystem.create(path);
+        InputStream inputStream =
+                new BufferedInputStream(new FileInputStream(new File("E:\\hello.txt")));
+        IOUtils.copyBytes(inputStream,fsDataOutputStream,conf,true);
+    }
+
+    @Test
+    public void download() throws IOException{
+        Path path = new Path("/ooxx/hello.txt");
+        FSDataInputStream fsDataInputStream = fileSystem.open(path);
+        FileOutputStream fileOutputStream = new FileOutputStream(new File("E:\\aaa.txt"));
+        IOUtils.copyBytes(fsDataInputStream,fileOutputStream,conf,true);
+    }
+
+    @Test
+    public void blocks() throws IOException{
+        Path path = new Path("/ooxx/hello.txt");
+        FileStatus fileStatus = fileSystem.getFileStatus(path);
+        BlockLocation[] fileBlockLocations = fileSystem.getFileBlockLocations(path, 0, fileStatus.getLen());
+        for (BlockLocation fileBlockLocation : fileBlockLocations) {
+            System.out.println(fileBlockLocation);
+
         }
     }
 }
