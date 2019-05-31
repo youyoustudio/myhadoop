ad71dddeeabdd12e2896f6759164df365cd03ac2
luwg <luwg@zzlc.com>
2019-05-31 17:38:57 +0800

    临时提交

--- src/main/java/com/youyoustudio/myhadoop/tq/MyWeather.java
+++ src/main/java/com/youyoustudio/myhadoop/tq/MyWeather.java
@@ -1,4 +1,13 @@
 package com.youyoustudio.myhadoop.tq;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapreduce.Job;
+
 public class MyWeather {
+    public static void main(String[] args) throws Exception{
+        Configuration configuration = new Configuration(true);
+        Job job = Job.getInstance(configuration);
+        job.setJarByClass(MyWeather.class);
+
+    }
 }
