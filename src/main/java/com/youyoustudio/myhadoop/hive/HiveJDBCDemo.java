76d1e998d1cde78f9c1bd6533c949585ed1dce26
luwg <luwg@zzlc.com>
2019-06-06 17:02:43 +0800

    hive jdbc

---
+++ src/main/java/com/youyoustudio/myhadoop/hive/HiveJDBCDemo.java
@@ -0,0 +1,9 @@
+package com.youyoustudio.myhadoop.hive;
+
+public class HiveJDBCDemo {
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+    public static void main(String[] args) {
+        //org.apache.hive.jdbc.HiveDriver driver;
+
+    }
+}
